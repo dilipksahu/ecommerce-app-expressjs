449f986f43836312bdfe8605f87f1597ab98869f
dilipksahu <sahud048@gmail.com>
2024-06-27 23:26:34 +0530

    test file commented code removed

--- test/orderService.test.js
+++ test/orderService.test.js
@@ -4,9 +4,6 @@ const orderService = require('../services/orderService');
 
 require('../mocks/paymentGatewayMock');
 require('../mocks/logisticsProviderMock');
-// const { processPayment, createShipment } = require('../services/orderService');
-// import orderService from '../services/orderService.js';
-
 
 describe('Order Service', () => {
     it('should process payment successfully', async () => {
@@ -21,23 +18,3 @@ describe('Order Service', () => {
     });
 });
 
-// Self-invoking async function to handle dynamic import and define tests
-// (async () => {
-//     const { expect } = await import('chai');
-  
-//     describe('Order Service', () => {
-//       it('should process payment successfully', async () => {
-//         const paymentResponse = await orderService.processPayment({ amount: 2000, currency: 'usd' });
-//         expect(paymentResponse).to.have.property('status', 'succeeded');
-//       });
-  
-//       it('should create shipment successfully', async () => {
-//         const shipmentResponse = await orderService.createShipment({ order_id: '12345' });
-//         expect(shipmentResponse).to.have.property('status', 'created');
-//         expect(shipmentResponse).to.have.property('tracking_number');
-//       });
-//     });
-  
-//     // Run the tests
-//     run();
-//   })();
